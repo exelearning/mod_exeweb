be9be222f5d76df825523689251e298f5aa1f5be
Juan Carrera <neticainformatica@gmail.com>
2023-11-17 17:26:03 +0100

    Bump version.

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2023110700;       // The current module version (Date: YYYYMMDDXX).
+$plugin->version   = 2023111700;       // The current module version (Date: YYYYMMDDXX).
 $plugin->requires  = 2022111800;    // Requires this Moodle version.
 $plugin->component = 'mod_exeweb'; // Full name of the plugin (used for diagnostics).
 $plugin->cron      = 0;
